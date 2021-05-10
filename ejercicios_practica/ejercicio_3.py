1c6a4dc9fca118c01f9a8618fd415fe4c44cc913
carlosandrada <83506321+carlosandrada@users.noreply.github.com>
2021-05-10 12:50:04 -0300

    Add files via upload

--- ejercicios_practica/ejercicio_3.py
+++ ejercicios_practica/ejercicio_3.py
@@ -10,17 +10,26 @@
 # Ejemplos variables de texto
 
 # Ingrese primero su nombre y luego su apellido
+texto_1 = 'carlos'
+texto_2 = 'andrada'
+
 # Capture ambos datos e imprima su nombre completo
+print (texto_1 , texto_2)
+
+
 print('Ingrese por consola su nombre/s:')
 nombre = str(input())
 
 print('Ingrese por consola su apellido/s:')
 apellido = str(input())
 
-# Imprima su nombre completo
 
 # Almacenar su nombre completo en una variable
 # nombre_completo = .....
-
+nombre_completo = 'carlos andrada'
 # Imprimir la cantidad de letras que posee su nombre completo
 # cantidad_letras = len(....)
+cantidad_letras = len(nombre_completo)
+print (nombre_completo , 'tiene' , cantidad_letras , 'caracteres')
+
+
